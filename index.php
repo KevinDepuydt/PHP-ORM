ca1f19413901fe29a4de352a6c95436a5c11fd4e
Kevin Depuydt <kevin.depuydt@supinternet.fr>
2015-12-11 23:17:14 +0100

    Add simple test on index

--- index.php
+++ index.php
@@ -8,16 +8,12 @@
 
 require_once 'vendor/autoload.php';
 
-use App\Orm\Orm, App\Entity\User;
+use App\Orm\Orm, App\Entity\User, App\Orm\QueryManager;
 
 Orm::init('127.0.0.1', 'orm', 'root', '');
 
-var_dump(Orm::getConnexion());
-
 $user = new User();
-$user->setLogin('login');
-$user->setEmail('mail@mail.fr');
-$user->setPassword(sha1('password'));
-$user->persist();
-
-var_dump($user);
+$user->setLogin('simple');
+$user->setEmail('simple@mail.fr');
+$user->setPassword(sha1('simple'));
+$user->save();
